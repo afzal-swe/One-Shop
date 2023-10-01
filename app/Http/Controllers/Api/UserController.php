e30e28ec0a0afd96f7984da1c5ab127baefa2244
afzal-swe <afzalbhola07@gmail.com>
2023-10-01 10:42:35 +0600

    Add Multiple user create API successfully

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Validator;
 class UserController extends Controller
 {
     //
+    // Single or Multi Data Show api function.::::::::::::::
     public function showUser($id = null)
     {
         // Don't any id than call this if condation after that show all user.
@@ -56,6 +57,60 @@ class UserController extends Controller
                 'created_at' => Carbon::now(),
             ]);
             $message =  'New User Added Successfully !';
+            return  response()->json(['message' => $message], 201); // 201 means createded suuccessfylly, kono data successfully create korla 201 ase
+        }
+    }
+
+
+    // Add Multi Data API Function :::::::::::::::::::::::::::::::::::::::::
+    public function addMultipleUser(Request $request)
+    {
+        if ($request->ismethod('post')) {
+
+            $rules = [
+                'users.*.name' => 'required',
+                'users.*.user_name' => 'required',
+                'users.*.phone' => 'required',
+                'users.*.password' => 'required',
+                'users.*.email' => 'required|email|unique:users',
+            ];
+
+            $coustomMessage = [
+                'users.*.name.required' => 'Name is required',
+                'users.*.email.required' => 'Email is required',
+                'users.*.email.email' => 'Email must be a valid email',
+                'users.*.password.required' => 'Password is required',
+                'users.*.user_name.required' => 'user_name is required',
+                'users.*.phone.required' => 'phone is required',
+            ];
+
+            $validator = Validator::make($request, $rules, $coustomMessage);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 422); // 422 means Error;
+            }
+
+            // User::insert([
+            //     'name' => $request->name,
+            //     'user_name' => $request->user_name,
+            //     'email' => $request->email,
+            //     'phone' => $request->phone,
+            //     'password' => $request->password,
+            //     'created_at' => Carbon::now(),
+            // ]);
+            // $message =  'New User Added Successfully !';
+            // return  response()->json(['message' => $message], 201); // 201 means createded suuccessfylly, kono data successfully create korla 201 ase
+
+            foreach ($request['users'] as $adduser) {
+                $user = new User();
+                $user->name = $adduser['name'];
+                $user->user_name = $adduser['user_name'];
+                $user->email = $adduser['email'];
+                $user->phone = $adduser['phone'];
+                $user->password = bcrypt($adduser['password']);
+                $user->save();
+                $message = "Multiple User Added Successfully";
+            }
             return  response()->json(['message' => $message], 201); // 201 means createded suuccessfylly, kono data successfully create korla 201 ase
         }
     }
--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Single or Multi Data Show api Route.::::::::::::::
 Route::get('/users/{id?}', [UserController::class, 'showUser']); // id ? = ? means optional , we are show single user or multi user showing. (get/fetch)
+// create User api function :::::::::::::::::::::::::
 Route::post('/users/store', [UserController::class, 'store']);
+// Add Multi Data API Function ::::::::::::::::::::::
+Route::post('/add-multiple-user', [UserController::class, 'addMultipleUser']);
 
 // Brand Route Section Start ==============================================================
 Route::get('/brands/{id?}', [BrandApiController::class, 'index']);
