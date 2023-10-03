386f7b9e0dff87831295121ebeea4d436f2fb63f
afzal-swe <afzalbhola07@gmail.com>
2023-10-03 11:17:01 +0600

    Make API CRUD for User table

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -84,7 +84,7 @@ class UserController extends Controller
                 'users.*.phone.required' => 'phone is required',
             ];
 
-            $validator = Validator::make($request, $rules, $coustomMessage);
+            $validator = Validator::make($request['users'], $rules, $coustomMessage);
 
             if ($validator->fails()) {
                 return response()->json($validator->errors(), 422); // 422 means Error;
@@ -112,6 +112,107 @@ class UserController extends Controller
                 $message = "Multiple User Added Successfully";
             }
             return  response()->json(['message' => $message], 201); // 201 means createded suuccessfylly, kono data successfully create korla 201 ase
+        }
+    }
+
+    // Update User Info API Function
+    public function UpdateUserInfo(Request $request, $id)
+    {
+        if ($request->ismethod('put')) {
+
+            $rules = [
+                'name' => 'required',
+            ];
+
+            $coustomMessage = [
+                'name.required' => 'Name is required',
+            ];
+
+            $validator = Validator::make($rules, $coustomMessage);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 422); // 422 means Error;
+            }
+
+            User::findOrFail($id)->update([
+                'name' => $request->name,
+                'user_name' => $request->user_name,
+                'phone' => $request->phone,
+                'updated_at' => Carbon::now(),
+            ]);
+            $message =  'User Update Successfully !';
+            return  response()->json(['message' => $message], 202); // 201 means createded suuccessfylly, kono data successfully update korla 202 ase
+        }
+    }
+    // Patch Api for Update User Info API Function
+    public function UpdateSingleRecord(Request $request, $id)
+    {
+        if ($request->ismethod('patch')) {
+
+            $rules = [
+                'name' => 'required',
+            ];
+
+            $coustomMessage = [
+                'name.required' => 'Name is required',
+            ];
+
+            $validator = Validator::make($rules, $coustomMessage);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 422); // 422 means Error;
+            }
+
+            User::findOrFail($id)->update([
+                'name' => $request->name,
+                'user_name' => $request->user_name,
+                'phone' => $request->phone,
+                'updated_at' => Carbon::now(),
+            ]);
+            $message =  'User Update Single Record Successfully !';
+            return  response()->json(['message' => $message], 202); // 201 means createded suuccessfylly, kono data successfully update korla 202 ase
+        }
+    }
+    // Delete API for Delete Single user Function
+    public function DeleteSingleRecord($id = null)
+    {
+        User::findOrFail($id)->delete();
+
+        $message =  'Single User Delete Successfully !';
+        return  response()->json(['message' => $message], 200); // 201 means createded suuccessfylly, kono data successfully ok korla 200 ase
+    }
+
+    // Delete Api for Delete Multiple user
+    public function DeleteMultipleRecord($ids)
+    {
+        $ids = explode(',', $ids);
+        User::whereIn('id', $ids)->delete();
+
+        $message =  'Multiple User Delete Successfully !';
+        return  response()->json(['message' => $message], 200); // 201 means createded suuccessfylly, kono data successfully ok korla 200 ase
+    }
+
+    // Delete API for Delete Single user with Json formet Function
+    public function DeleteSingleRecordJson(Request $request)
+    {
+        if ($request->ismethod('delete')) {
+            $data = $request->all();
+            User::where('id', "=", $data['id'])->delete();
+
+            $message =  'Single User Delete With Json Formet Successfully !';
+            return  response()->json(['message' => $message], 200); // 201 means createded suuccessfylly, kono data successfully ok korla 200 ase
+        }
+    }
+
+    // Delete API for delete multiple user with json formate.
+    public function DeleteMultipleRecordJson(Request $request)
+    {
+        if ($request->ismethod('delete')) {
+            $data = $request->all();
+            User::whereIn('id', $data['ids'])->delete();
+
+            $message =  'Multiple User Delete With Json Formate Successfully !';
+            return  response()->json(['message' => $message], 200); // 201 means createded suuccessfylly, kono data successfully ok korla 200 ase
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,8 +25,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/users/{id?}', [UserController::class, 'showUser']); // id ? = ? means optional , we are show single user or multi user showing. (get/fetch)
 // create User api function :::::::::::::::::::::::::
 Route::post('/users/store', [UserController::class, 'store']);
-// Add Multi Data API Function ::::::::::::::::::::::
+// Add Multi Data API Roure ::::::::::::::::::::::
 Route::post('/add-multiple-user', [UserController::class, 'addMultipleUser']);
+// Patch API for Update Route Multiple data::::::::::::::::::::::
+Route::put('/update-user-info/{id}', [UserController::class, 'UpdateUserInfo']);
+// Put API for Update Route ::::::::::::::::::::::
+Route::patch('/update-single-record/{id}', [UserController::class, 'UpdateSingleRecord']);
+// Delete API for Delete Single user Route ::::::::::::::::::::::
+Route::delete('/delete-single-record/{id}', [UserController::class, 'DeleteSingleRecord']);
+// Delete API for Delete Single user with json Route ::::::::::::::::::::::
+Route::delete('/delete-single-record-with-json', [UserController::class, 'DeleteSingleRecordJson']);
+// Delete API for Delete Multiple user Route ::::::::::::::::::::::
+Route::delete('/delete-multiple-record/{ids}', [UserController::class, 'DeleteMultipleRecord']);
+// Delete API for Delete Multiple user with json Route ::::::::::::::::::::::
+Route::delete('/delete-multiple-record-with-json', [UserController::class, 'DeleteMultipleRecordJson']);
 
 // Brand Route Section Start ==============================================================
 Route::get('/brands/{id?}', [BrandApiController::class, 'index']);
